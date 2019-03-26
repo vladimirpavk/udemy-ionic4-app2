7aae03c8f01ba3d37a667636b41bd86919f9f891
Vladimir Pavković <vladimirpavk@gmail.com>
2019-03-26 11:10:28 +0100

    +

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,16 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: '', redirectTo: 'auth', pathMatch: 'full' },
+  { path: 'auth', loadChildren: './auth/auth.module#AuthPageModule' },
+  { path: 'places', loadChildren: './places/places.module#PlacesPageModule' },
+  { path: 'discover', loadChildren: './places/discover/discover.module#DiscoverPageModule' },
+  { path: 'offers', loadChildren: './places/offers/offers.module#OffersPageModule' },
+  { path: 'new-offer', loadChildren: './places/offers/new-offer/new-offer.module#NewOfferPageModule' },
+  { path: 'edit-offer', loadChildren: './places/offers/edit-offer/edit-offer.module#EditOfferPageModule' },
+  { path: 'place-detail', loadChildren: './places/offers/place-detail/place-detail.module#PlaceDetailPageModule' },
+  { path: 'offer-bookings', loadChildren: './offers/offer-bookings/offer-bookings.module#OfferBookingsPageModule' },
+  { path: 'bookings', loadChildren: './bookings/bookings.module#BookingsPageModule' }
 ];
 
 @NgModule({
