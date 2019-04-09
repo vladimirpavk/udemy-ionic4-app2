df3f69c379f2b02b68a968eaf0c540504ef386ff
Vladimir Pavković <vladimirpavk@gmail.com>
2019-04-09 14:56:39 +0200

    +

--- src/app/places/offers/offers.page.ts
+++ src/app/places/offers/offers.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PlacesService } from '../places.service';
 import { Place } from '../place.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-offers',
@@ -11,10 +12,21 @@ export class OffersPage implements OnInit {
 
   private _places:Place[];
 
-  constructor(private _placesService:PlacesService) { }
+  constructor(
+    private _placesService:PlacesService,
+    private _router:Router
+  ) { }
 
   ngOnInit() {
       this._places = this._placesService.getPlaces();
+  }
+
+  private trashIconClicked(place:Place):void{
+    console.log(place);
+  }
+
+  private itemClicked(place:Place):void{
+    this._router.navigate(['/', 'places', 'tabs', 'offers', 'offer-bookings', place.id]);
   }
 
 }
