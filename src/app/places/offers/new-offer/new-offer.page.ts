34fd61a4b472992d478e8c376f71695cec961482
Vladimir Pavković <vladimirpavk@gmail.com>
2019-04-19 13:54:52 +0200

    +

--- src/app/places/offers/new-offer/new-offer.page.ts
+++ src/app/places/offers/new-offer/new-offer.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-new-offer',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewOfferPage implements OnInit {
 
+  private _offerForm: FormGroup;
+
   constructor() { }
 
   ngOnInit() {
+    this._offerForm=new FormGroup({
+      'place': new FormGroup({
+        'title': new FormControl(),
+        'description': new FormControl(),
+        'imageUrl': new FormControl(),
+        'price': new FormControl()
+      }),
+      'startDate': new FormControl(),
+      'endDate': new FormControl()
+    })
+  }
+
+  private formSubmitted(){
+    console.log(this._offerForm);
   }
 
 }
