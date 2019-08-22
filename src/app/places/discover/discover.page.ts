bff210aa06e1774c3dbd660dc0356f645e88514f
Vladimir Pavković <vladimirpavk@gmail.com>
2019-08-22 14:43:34 +0200

    +

--- src/app/places/discover/discover.page.ts
+++ src/app/places/discover/discover.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
 import { PlacesService } from '../places.service';
 import { Place } from '../place.model';
 import { Common } from '../../common/common';
-import { Subscription, Observable } from 'rxjs';
+import { Observable, from, asyncScheduler, queueScheduler } from 'rxjs';
 import { UIService } from '../../common/ui.service';
 
 @Component({
@@ -10,7 +10,7 @@ import { UIService } from '../../common/ui.service';
   templateUrl: './discover.page.html',
   styleUrls: ['./discover.page.scss'],
 })
-export class DiscoverPage{
+export class DiscoverPage implements OnInit{
   
   private _places$:Observable<{favoritePlace:Place, otherPlaces:Place[]}>;  
 
@@ -21,6 +21,111 @@ export class DiscoverPage{
 
   ionViewWillEnter(){
     //this._places$ = this._placesService.discoveredPlaces;
+  }
+
+  ngOnInit(){
+    /* let observable1 = from<Number>([1,2,3], asyncScheduler);
+
+    observable1.subscribe(
+      (value:Number)=>{
+        console.log(value);
+      }
+    );
+
+    console.log('before subscribe'); */
+
+    /* let observable2 = new Observable(
+      (observer)=>{
+        observer.next(1);
+        setTimeout(
+          ()=>{
+            observer.next(2);
+            setTimeout(
+              ()=>{
+                observer.next(3);
+                observer.complete();
+              }, 1000);
+          }, 2000);
+      }
+    );
+
+    observable2.subscribe({
+      next: (value:number)=>{
+        console.log(value);
+      },
+      complete: ()=>console.log('Stream completed...')
+    }); */
+
+    /* let timeNow = Date.now();
+
+    queueScheduler.schedule(
+      (value:number)=>{
+        console.log(`${queueScheduler.now()-timeNow}ms`, 'queueScheduler - '+value);
+      }, 500, 1
+    );
+
+    queueScheduler.schedule(
+      (value:number)=>{
+        console.log(`${queueScheduler.now()-timeNow}ms`, 'queueScheduler - '+value);
+      }, 500, 2
+    );
+
+    queueScheduler.schedule(
+      (value:number)=>{
+        console.log(`${queueScheduler.now()-timeNow}ms`, 'queueScheduler - '+value);
+      }, 500, 3
+    );
+
+    from([1,2,3], queueScheduler).subscribe(
+      (value)=>console.log(value)
+    ); */
+    
+    /* timeNow = Date.now();
+
+    asyncScheduler.schedule(
+      (value:number)=>{
+        console.log(`${asyncScheduler.now()-timeNow}ms`, 'asyncScheduler - '+value);
+      }, 500, 1
+    );
+
+    asyncScheduler.schedule(
+      (value:number)=>{
+        console.log(`${asyncScheduler.now()-timeNow}ms`, 'asyncScheduler - '+value);
+      }, 1000, 2
+    );
+
+    asyncScheduler.schedule(
+      (value:number)=>{
+        console.log(`${asyncScheduler.now()-timeNow}ms`, 'asyncScheduler - '+value);
+      }, 500, 3
+    );
+
+    asyncScheduler.schedule(
+      (value:number)=>{
+        console.log(`${asyncScheduler.now()-timeNow}ms`, 'asyncScheduler - '+value);
+      }, 750, 3
+    );
+
+    from() */
+
+    let observableX = new Observable(
+      (observer)=>{
+        let timeNow=Date.now();
+        queueScheduler.schedule(
+          (value)=>{
+            observer.next(`${queueScheduler.now()-timeNow}ms `+ value);
+          }, 500, 1);
+        queueScheduler.schedule(
+          (value)=>{
+            observer.next(`${queueScheduler.now()-timeNow}ms ` + value);
+          }, 1000, 2);
+        queueScheduler.schedule(
+          (value)=>{
+            observer.next(`${queueScheduler.now()-timeNow}ms ` + value);
+          }, 500, 3);
+      }).subscribe(
+        (value)=>console.log(value)
+      );
   }
 
   private segmentChanged(event: CustomEvent){
