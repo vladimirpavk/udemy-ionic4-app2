08f925d0f01ab4396cb8b022174d53f1fb8158f9
Vladimir Pavković <vladimirpavk@gmail.com>
2019-09-04 13:47:16 +0200

    +

--- src/app/shared/location-picker/location-picker.component.ts
+++ src/app/shared/location-picker/location-picker.component.ts
@@ -1,3 +1,5 @@
+/// <reference types="@types/googlemaps" />
+
 import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 
@@ -17,10 +19,30 @@ export class LocationPickerComponent implements OnInit, AfterViewInit {
   ngOnInit() {}
 
   ngAfterViewInit() {
-    const map = new google.maps.Map(this.mapDivElementRef.nativeElement, {
-      center: {lat: -34.397, lng: 150.644},
-      zoom: 8
-    });
+    window.navigator.geolocation.getCurrentPosition(
+      (position:Position)=>{
+        //console.log('current position', position);
+        const map = new google.maps.Map(this.mapDivElementRef.nativeElement, {
+          center: {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          },
+          zoom: 20
+        });
+        map.addListener('click', (eventArgs)=>{
+          //console.log(eventArgs);
+          this._modalCtrl.dismiss(
+            {
+              lat: eventArgs.latLng.lat(),
+              lng: eventArgs.latLng.lng()
+            }
+          )
+        });        
+      },
+      ()=>{
+        console.error('Current position could not be determined...');
+      }
+    );   
   }
 
   private dismissModal(){
